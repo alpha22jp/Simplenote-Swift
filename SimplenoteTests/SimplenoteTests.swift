f94fe4b363567b375e399d01f7423b82f716bbf8
alpha22jp <alpha22jp@gmail.com>
2016-01-11 21:22:14 +0900

    xcode7.0の自動生成コードに合わせる (不要？)

--- SimplenoteTests/SimplenoteTests.swift
+++ SimplenoteTests/SimplenoteTests.swift
@@ -6,8 +6,8 @@
 //  Copyright (c) 2015 alpha22jp@gmail.com. All rights reserved.
 //
 
-import UIKit
 import XCTest
+@testable import Simplenote
 
 class SimplenoteTests: XCTestCase {
     
@@ -23,12 +23,12 @@ class SimplenoteTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
